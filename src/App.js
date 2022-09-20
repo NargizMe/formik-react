e46b248d229a682db02674bad8926a0a0eed7db8
Nargiz Rafiyeva <nargiz.pr@code.local>
2022-09-20 16:11:41 +0400

    yup added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 function App() {
   const [category, setCategory] = useState([]);
@@ -14,42 +15,65 @@ function App() {
     })()
   }, [])
 
-  function handleOptionClick(e){
-    // setCategoryId(e.target.value)
-    // console.log(va);
-  }
+   
+  const addProduct = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    unitPrice: Yup.number()
+      .required('Required'),
+    unitsInStock: Yup.number()
+      .required('Required'),
+    quantityPerUnit: Yup.number()
+    .required('Required'),
+  });
 
   return (
     <div>
      <h1>Add Product Form</h1>
      <h2>{categoryId}</h2>
      <Formik
-       initialValues={{ name: '', categoryId: '', unitPrice: '', unitsInStock: '', unitsOnOrder: '', discontinued: '',reorderLevel: '', quantityPerUnit: '', categoryId: '' }}
+       initialValues={{ name: '', categoryId: '', unitPrice: 0, unitsInStock: 0, unitsOnOrder: '', discontinued: false, reorderLevel: '', quantityPerUnit: '', categoryId: '' }}
+       validationSchema={addProduct}
        onSubmit={async(values) => {
         await axios.post('https://northwind.vercel.app/api/products', values)
         setCategoryId(values.categoryId);
-        console.log(values);
        }}
      >
-      <Form>
-        <Field as="select" name="categoryId">
+      {({ errors, touched }) => {
+        return <Form>
+          <Field as="select" name="categoryId">
+            {
+              category.map((el) => {
+                return <option value={el.id} key={el.id}>{el.name}</option>
+              })
+            }
+          </Field>
+          <Field type="text" name="name" placeholder = 'name' />
           {
-            category.map((el) => {
-              return <option value={el.id} key={el.id}>{el.name}</option>
-            })
+            errors.name && <span style={{color:'tomato'}}>{errors.name}</span>
           }
-        </Field>
-        <Field type="text" name="name" placeholder = 'name' />
-        <Field type="text" name="unitPrice" placeholder = 'unitPrice'/>
-        <Field type="text" name="unitsInStock" placeholder = 'unitsInStock' />
-        <Field type="text" name="unitsOnOrder" placeholder = 'unitsOnOrder' />
-        <Field type="text" name="discontinued" placeholder = 'discontinued' />
-        <Field type="text" name="reorderLevel" placeholder = 'reorderLevel' />
-        <Field type="text" name="quantityPerUnit" placeholder = 'quantityPerUnit' />
-        <button type="submit" >
-          Submit
-        </button>
-      </Form>
+          <Field type="number" name="unitPrice" placeholder = 'unitPrice'/>
+          {
+            errors.unitPrice && <span style={{color:'tomato'}}>{errors.unitPrice}</span>
+          }
+          <Field type="number" name="unitsInStock" placeholder = 'unitsInStock' />
+          {
+            errors.unitsInStock && <span style={{color:'tomato'}}>{errors.unitsInStock}</span>
+          }
+          <Field type="text" name="unitsOnOrder" placeholder = 'unitsOnOrder' />
+          <Field type="checkbox" name="discontinued" />
+          <Field type="text" name="reorderLevel" placeholder = 'reorderLevel' />
+          <Field type="text" name="quantityPerUnit" placeholder = 'quantityPerUnit' />
+          {
+            errors.quantityPerUnit && <span style={{color:'tomato'}}>{errors.quantityPerUnit}</span>
+          }
+          <button type="submit" >
+            Submit
+          </button>
+        </Form>
+        }}
      </Formik>
    </div>
   );
